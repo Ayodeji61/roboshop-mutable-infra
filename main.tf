0a51157945bb3f7d37c2ed49d82d8dd02517d0d6
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-18 05:13:06 -0700

    Updating

--- main.tf
+++ main.tf
@@ -16,14 +16,21 @@ module "docdb" {
   name = each.key
   engine   = each.value.engine
   env    = var.env
-  subnets = [for i, j in module.vpc : j.private_subnets["app"]["subnets"][*].id]
+  subnets = flatten([for i, j in module.vpc : j.private_subnets["app"]["subnets"][*].id])
 }
 
-/*
+
 module "rds" {
   source = "./vendor/modules/rds"
   rds    = var.rds
   env    = var.env
-  subnets = local.database_private_subnets[*].id
+  subnets = flatten([for i, j in module.vpc : j.private_subnets["app"]["subnets"][*].id])
+  allocated_storage = 10
+  engine = "aurora-mysql"
+  engine_version = "5.7.mysql_aurora.2.03.2"
+  instance_class = "db.t3.micro"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot = true
+
 }
-*/
+
