7f4499dd1cf03872a5428ba7d0e7514a507938b3
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-21 17:50:05 -0700

    Updating

--- main.tf
+++ main.tf
@@ -77,6 +77,7 @@ module "apps" {
   private_zone_id = var.private_zone_id
   public_zone_id = var.public_zone_id
   public_dns_name      = try(each.value.public_dns_name, null)
+  ACM_ARN             =      var.ACM_ARN
 }
 
 
@@ -91,5 +92,4 @@ module "alb" {
   vpc_id          = element([for i, j in module.vpc : j.vpc_id], 0)
   vpc_cidr        = element([for i, j in module.vpc : j.vpc_cidr], 0)
   internal        = each.value.internal
-  ACM_ARN         = var.ACM_ARN
 }
